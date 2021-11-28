0f026fa943b15aad806d8b42c5179455ba71a5d0
Gregory Banks <gbanks1438@gmail.com>
2021-11-28 14:43:25 -0500

    commit tree 980d28694c5bac6a9a4509147ce596404498f129

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+    function notLooking() {
+        console.log("Hey! Eyes on me!");
+      }
+      function looking() {
+        console.log("Good!");
+      }
+return (
+    <button onFocus={looking} onBlur={notLooking}>Eyes on me</button>
+)
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad () {
+    function whenChanged() {
+        console.log("Entering password...");
+      }
+    return (
+    <form>
+  <input type="password" onChange={whenChanged} />
+</form>
+    );
+}
+
+export default Keypad;
